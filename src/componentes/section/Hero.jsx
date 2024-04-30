19f3cc847ad336324a43d63709183413fa1c949c
Alexsandro Soares Teixeira <notbooksoares@gmail.com>
2024-04-30 09:29:19 -0300

    foi implementado o componente btn junto com seu prop text e sua estilizacao

--- src/componentes/section/Hero.jsx
+++ src/componentes/section/Hero.jsx
@@ -1,9 +1,14 @@
-//import style from 'Hero.module.css'
+import style from "./Hero.module.css"
+import Btn from "../elements/ButtonFade"
 
-function Hero(){
-    return(
-        <div>
-            Hero
+function Hero() {
+    return (
+        <div className={style.Hero}>
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing <br />
+                elit, sed do eiusmod tempor incididunt ut labore et <br />
+                dolore magna aliqua. </p>
+            <Btn text={'Saber mais'}/>
         </div>
     )
 }
