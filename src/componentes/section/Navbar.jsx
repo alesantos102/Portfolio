bdf7ab74a83498f31aa940fd9b002c990761ebea
Alexsandro Soares Teixeira <notbooksoares@gmail.com>
2024-05-04 15:41:38 -0300

    Footer incrementado

--- src/componentes/section/Navbar.jsx
+++ src/componentes/section/Navbar.jsx
@@ -7,18 +7,18 @@ function Navbar() {
     return (
 
         <div className={styles.Navbar}>
-            <div className={styles.icons}></div>
-            <div className={styles.content}>
+            <span/>
+            <div>
                 <ul>
                     <li><p>Projetos</p></li>
                     <li><p>Tecnologias</p></li>
                     <li><p>Sobre mim</p></li>
                 </ul>
             </div>
-            <div className={styles.icons}>
+            <div>
                 <ul>
                     <li> <img src={github} alt="github" /></li>
-                    <li><img className={styles.invert} src={linkedin} alt="linkedin"  /></li>
+                    <li><img src={linkedin} alt="linkedin" /></li>
                 </ul>
             </div>
         </div>
